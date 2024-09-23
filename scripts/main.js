b447b3442486383da1918bb7d4190025210946e3
Himu004 <himu.chowdhury01@gmail.com>
2024-09-23 14:55:28 +0600

    Blog Page redesigned

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
-console.log('Jellow Ayaaz ......');
+let totalBalance = getElementsId("total-balance");
+
+console.log(totalBalance.innerText);
+
+function card1Donation() {
+    getElementsId("btn-1").addEventListener("click", function () {
+   
+        const inputAmount = getInputValue("input-1");
+        const amount = parseInt(inputAmount);
+        const totalDonation = getElementsId("card-1-donation");
+        const valueTotal = totalDonation.innerText;
+        const totalDonationNumber = parseInt(valueTotal);
+      
+        const newTotal = totalDonationNumber + amount;
+        totalDonation.innerText = newTotal;
+      
+        const myBalance = parseInt(totalBalance.innerText);
+        const newBalance = myBalance - amount;
+        totalBalance.innerText = newBalance;
+      
+          
+        my_modal_1.showModal()
+      
+        
+      });
+}
+
+card1Donation();
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,1 +1,8 @@
-console.log('Jellow Ayaaz ......');
+function getElementsId(element) {
+    return document.getElementById(element);
+}
+
+function getInputValue(element) {
+    element = getElementsId(element);
+    return element.value
+}
