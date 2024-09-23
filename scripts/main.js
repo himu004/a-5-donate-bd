1f4e7417958bb8b4c8b5014dc90377fdcfb5ccfc
Himu004 <himu.chowdhury01@gmail.com>
2024-09-23 18:24:33 +0600

    History, toggleButton and other functionality added

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,134 @@
-let totalBalance = getElementsId("total-balance");
+let myTotalBalance = getElementsInINT("total-balance");
+console.log(myTotalBalance);
 
-console.log(totalBalance.innerText);
+const historyBtn = getElementsId("history-btn");
+const donationBtn = getElementsId("donation-btn");
+
+// sections
+const donationSection = getElementsId("donation");
+const historySection = getElementsId("history");
+
+// menu btn click handles
+historyBtn.addEventListener("click", function () {
+  donationSection.classList.add("hidden");
+  historySection.classList.remove("hidden");
+
+  historyBtn.classList.add("bg-[#B4F461]", "text-black");
+  historyBtn.classList.remove(
+    "border-2",
+    "border-[#111111B3]",
+    "text-[#111111B3]"
+  );
+
+  donationBtn.classList.remove("bg-[#B4F461]", "text-black", "btn-outline");
+  donationBtn.classList.add(
+    "border-2",
+    "border-[#111111B3]",
+    "text-[#111111B3]"
+  );
+});
+
+donationBtn.addEventListener("click", function () {
+  donationSection.classList.remove("hidden");
+  historySection.classList.add("hidden");
+
+  donationBtn.classList.add("bg-[#B4F461]", "text-black");
+  donationBtn.classList.remove(
+    "border-2",
+    "border-[#111111B3]",
+    "text-[#111111B3]"
+  );
+
+  historyBtn.classList.remove("bg-[#B4F461]", "text-black", "btn-outline");
+  historyBtn.classList.add(
+    "border-2",
+    "border-[#111111B3]",
+    "text-[#111111B3]"
+  );
+});
 
 function card1Donation() {
-    getElementsId("btn-1").addEventListener("click", function () {
-   
-        const inputAmount = getInputValue("input-1");
-        const amount = parseInt(inputAmount);
-        const totalDonation = getElementsId("card-1-donation");
-        const valueTotal = totalDonation.innerText;
-        const totalDonationNumber = parseInt(valueTotal);
+  getElementsId("btn-1").addEventListener("click", function () {
+
+    const inputAmount = getInputValue("input-1");
+
+    console.log(inputAmount);
+    if (inputAmount < 0 || isNaN(inputAmount)) {
+      return alert("Please Give Valid Input");
+    } else if (myTotalBalance < inputAmount) {
+      return alert("You Do Not Have Sufficient Balance");
+    } else {
+      const localTotalDonations = getElementsInINT("card-1-donation");
+
+      const newTotalDonated = inputAmount + localTotalDonations;
+      document.getElementById("card-1-donation").innerText = newTotalDonated;
+
+      const newTotalBalance = myTotalBalance - inputAmount;
+      getElementsId("total-balance").innerText = newTotalBalance;
+
       
-        const newTotal = totalDonationNumber + amount;
-        totalDonation.innerText = newTotal;
-      
-        const myBalance = parseInt(totalBalance.innerText);
-        const newBalance = myBalance - amount;
-        totalBalance.innerText = newBalance;
-      
-          
-        my_modal_1.showModal()
-      
+    }
+    my_modal_1.showModal();
+    getElementsId("input-1").value = "";
+    historyCreation(inputAmount, 'card-1-tittle')
+  });
+}
+function card2Donation() {
+    getElementsId("btn-2").addEventListener("click", function () {
+  
+      const inputAmount = getInputValue("input-2");
+  
+      console.log(inputAmount);
+      if (inputAmount < 0 || isNaN(inputAmount)) {
+        return alert("Please Give Valid Input");
+      } else if (myTotalBalance < inputAmount) {
+        return alert("You Do Not Have Sufficient Balance");
+      } else {
+        const localTotalDonations = getElementsInINT("card-1-donation");
+  
+        const newTotalDonated = inputAmount + localTotalDonations;
+        document.getElementById("card-2-donation").innerText = newTotalDonated;
+  
+        const newTotalBalance = myTotalBalance - inputAmount;
+        getElementsId("total-balance").innerText = newTotalBalance;
+  
         
-      });
+      }
+      my_modal_1.showModal();
+      getElementsId("input-2").value = "";
+      historyCreation(inputAmount, 'card-2-tittle')
+    });
+}
+
+function card3Donation() {
+    getElementsId("btn-3").addEventListener("click", function () {
+  
+      const inputAmount = getInputValue("input-3");
+  
+      console.log(inputAmount);
+      if (inputAmount < 0 || isNaN(inputAmount)) {
+        return alert("Please Give Valid Input");
+      } else if (myTotalBalance < inputAmount) {
+        return alert("You Do Not Have Sufficient Balance");
+      } else {
+        const localTotalDonations = getElementsInINT("card-3-donation");
+  
+        const newTotalDonated = inputAmount + localTotalDonations;
+        document.getElementById("card-3-donation").innerText = newTotalDonated;
+  
+        const newTotalBalance = myTotalBalance - inputAmount;
+        getElementsId("total-balance").innerText = newTotalBalance;
+  
+        
+      }
+      my_modal_1.showModal();
+      getElementsId("input-3").value = "";
+      historyCreation(inputAmount, 'card-3-tittle')
+    });
 }
 
 card1Donation();
+card2Donation();
+card3Donation();
+
+
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,8 +1,31 @@
+function getElementsInINT(element) {
+    const stringElement = document.getElementById(element).innerText;
+    const intElement  = parseInt(stringElement);
+    return intElement;
+}
+
 function getElementsId(element) {
-    return document.getElementById(element);
+    return document.getElementById(element)
 }
 
 function getInputValue(element) {
     element = getElementsId(element);
-    return element.value
+    return parseInt(element.value)
+}
+
+function historyCreation(inputAmount, locationTitle){
+    const div = document.createElement('div');
+    const now = new Date()
+    const dateTimeString = now.toString();
+    const title = document.getElementById(locationTitle).innerText
+    div.classList.add( 'flex', 'flex-col', 'gap-4', 'md:p-8', 'p-4', 'border-2', 'border-gray-400', 'rounded-xl', 'm-5');
+    div.innerHTML = `
+        <h3 class="md:text-xl font-extrabold text-black">
+            ${inputAmount} Taka donated to: ${title}
+        </h3>
+        <p class="text-[#111111B3] bg-[#f9f7f3] rounded-lg px-4 py-2">Date : 
+            ${dateTimeString}
+        </p>   
+    `;
+    document.getElementById('history').appendChild(div);
 }
